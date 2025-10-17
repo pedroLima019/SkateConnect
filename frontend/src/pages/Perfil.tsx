9b8e0adace9e3e68ef1422650e55c9c28aa2b750
pedroLima019 <pedroliveiralima21@gmail.com>
2025-10-17 14:23:27 -0300

    fix(oerfil): improving the responsiveness of the post grid

--- frontend/src/pages/Perfil.tsx
+++ frontend/src/pages/Perfil.tsx
@@ -18,11 +18,11 @@ const Perfil: React.FC<PerfilProps> = ({ name, post, followers, follows }) => {
         <NavSidebar />
         <div className="flex-1 flex flex-col lg:ml-[260px]">
           <Navigation />
-          <main className="flex flex-col items-center w-full p-4 relative">
+          <main className="flex flex-col items-center w-full p-2 ">
             <section className="sticky top-0 z-20 bg-black w-full flex flex-col items-center gap-3.5 pb-3 pt-2 border-b border-zinc-800">
-              <div className="flex items-center justify-around w-full">
+              <div className="flex items-center justify-around w-full md:justify-center md:gap-10 ">
                 <div className="flex flex-col items-start justify-center">
-                  <UserIcon className="bg-white text-black rounded-full w-8 h-8 p-1 mb-1" />
+                  <UserIcon className="bg-white text-black rounded-full w-8 h-8 p-1 mb-1 md:mb-1.5" />
                   <p className="text-[12px] font-semibold">{name}</p>
                 </div>
                 <div className="flex flex-col items-center">
@@ -38,19 +38,30 @@ const Perfil: React.FC<PerfilProps> = ({ name, post, followers, follows }) => {
                   <p className="font-semibold text-[12px]">Seguidores</p>
                 </div>
               </div>
-              <div className="w-full flex justify-around items-center">
-                <button className="bg-zinc-500 p-1 rounded-xl w-[140px] text-[12px] font-semibold hover:bg-zinc-700">
+              <div className="w-full flex justify-center items-center gap-2.5">
+                <button className="bg-zinc-500 p-1 rounded-xl w-[140px] text-[12px] font-semibold hover:bg-zinc-700 md:w-[250px] md:p-2 ">
                   Editar Perfil
                 </button>
-                <button className="bg-zinc-500 p-1 rounded-xl w-[140px] text-[12px] font-semibold hover:bg-zinc-700">
+                <button className="bg-zinc-500 p-1 rounded-xl w-[140px] text-[12px] font-semibold hover:bg-zinc-700 md:w-[250px] md:p-2">
                   Compartilhar Perfil
                 </button>
               </div>
             </section>
             <section
               aria-label="Fotos do perfil"
-              className="grid grid-cols-3 gap-[2px] mt-4 w-full max-w-md overflow-y-auto"
-              style={{ maxHeight: "calc(100vh - 265px)" }}
+              className="
+                grid 
+                grid-cols-3 sm:grid-cols-4 
+                gap-[2px]
+                mt-4 
+                w-full 
+                max-w-7xl
+                overflow-y-auto
+                px-2
+              "
+              style={{
+                maxHeight: "calc(100dvh - clamp(180px, 25vh, 300px))",
+              }}
             >
               {Array.from({ length: 30 }).map((_, index) => (
                 <div
